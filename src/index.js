0b67fa4798d973d5f85cbf4c34d2e70a88badf9c
guchigow <lanov64@yandex.kz>
2023-01-03 04:44:01 +0600

    fix: eslint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.exports = function reverse (n) {
-    let str = Math.abs(n).toString();
-    let rev_arr =  str.split('').reverse();
-    return Number(rev_arr.join(''));
-}
+module.exports = function reverse(n) {
+  const str = Math.abs(n).toString();
+  const revArr = str.split('').reverse();
+  return Number(revArr.join(''));
+};
